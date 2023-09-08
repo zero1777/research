fc152492f3e3d69264d0c4ff2cd1fab70e1a3abf
zero1777 <tumi9325@gmail.com>
2023-09-08 15:59:50 +0800

    v1: renamed geometric -> graph

--- graph.py
+++ graph.py
@@ -2,7 +2,7 @@ import rkgb.src as rkgb
 from rkgb.utils.ast_add_on import ast_to_str
 
 # Purpose: parse the rkgb results into a class 
-class Geometric:
+class Graph:
     def __init__(
         self,
         original_model,
@@ -15,11 +15,11 @@ class Geometric:
         self.rkgb_results = rkgb.make_all_graphs(
             original_model, model_inputs, verbose=verbose, bool_kg=True
         )
-        self.kgraph_list = self.rkgb_results.K_graph_list
+        self.graph_list = self.rkgb_results.K_graph_list
         self.dict_constants = self.rkgb_results.K_graph_list[0].dict_constants
         self.eq_classes = self.rkgb_results.equivalent_classes
-        self.init_code = ast_to_str(self.kgraph_list[0].init_code)
-        self.output = self.kgraph_list[-1].output_kdn_data
+        self.init_code = ast_to_str(self.graph_list[0].init_code)
+        self.output = self.graph_list[-1].output_kdn_data
 
         
 
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 import torchvision.models as models
 from copy import deepcopy
 
-from geometric import Geometric
+from graph import Graph
 from utils import *
 from def_op import RunOp, DelOp, OpSchedule
 from compiler import Compiler, RngState, RK_Storage
@@ -23,7 +23,7 @@ class Asuta(torch.nn.Module):
     ):
         super().__init__()
         self.mode = mode # or "inference"
-        self.graph = Geometric(original_model, model_inputs)
+        self.graph = Graph(original_model, model_inputs)
         self.device = get_device()
         self.storage = RK_Storage(self.device, self.graph.model, self.graph.dict_constants)
         self.construct_op_list()
@@ -35,7 +35,7 @@ class Asuta(torch.nn.Module):
         self.fwd_op_list = []
         self.bwd_op_list = []
 
-        for kg in self.graph.kgraph_list:
+        for kg in self.graph.graph_list:
             users = {}
             op_list = []
             for kdn in kg.list_kdn:
@@ -79,14 +79,14 @@ class Asuta(torch.nn.Module):
             print(f'bwd_op: {op}')
 
         list_kdn = []
-        for kg in self.graph.kgraph_list:
+        for kg in self.graph.graph_list:
             list_kdn += kg.list_kdn
         
         self.op_sched = OpSchedule(
             self.fwd_op_list + self.bwd_op_list,
             None,
-            self.graph.kgraph_list[0].input_kdn_data,
-            self.graph.kgraph_list[0].input_kdn_grad,
+            self.graph.graph_list[0].input_kdn_data,
+            self.graph.graph_list[0].input_kdn_grad,
             self.graph.output,
             list_kdn,
         )
@@ -114,7 +114,7 @@ class Asuta(torch.nn.Module):
         
         exec(self.graph.init_code, self.storage.gd, self.storage.ld)
 
-        for kg in self.graph.kgraph_list:
+        for kg in self.graph.graph_list:
             for kdn in kg.list_kdn:
                 tensor_val = torch.empty(
                     0, device=self.device,
@@ -199,98 +199,10 @@ def verify(model1, model2, inputs, dict_kwargs=None):
 
     return same_train, same_eval, same_grad
 
-
-def test(model, sample):
-    for_rkgb = rkgb.make_all_graphs(model, sample, verbose=False, bool_kg=True)
-    kgraph_list = for_rkgb.K_graph_list
-
-    list_sols = []
-    fwd_op_list = []
-    for kg in kgraph_list:
-        op_list = []
-        users = {}
-        for kdn in kg.list_kdn:
-            users[kdn.name] = len(kdn.users_real)
-
-        for kcn in kg.list_kcn:
-            if "loss" in kcn.name:
-                op_list.append(RunOp(kcn))
-            op_list.append(RunOp(kcn))
-
-            for deps in kcn.deps_global:
-                if deps.name not in users:
-                    continue
-                if deps not in kcn.deps_fake:
-                    users[deps.name] -= 1
-                    if users[deps.name] == 0:
-                        op_list.append(DelOp(deps))
-
-        for op in op_list:
-            print(f'op: {op}')
-        
-
-
-        # loss_idx = 0
-        # for i, op in enumerate(op_list):
-        #     if "loss" in op.name:
-        #         loss_idx = i
-        #         break
-
-        # fwd_sched = OpSchedule(
-        #     op_list[:loss_idx+1],
-        #     None,
-        #     kg.input_kdn_data,
-        #     kg.input_kdn_grad,
-        #     kg.output_kdn_data,
-        #     kg.list_kdn,
-        # )
-
-        # list_sols.append(fwd_sched)
-
-    list_kdn = []
-    for kg in kgraph_list:
-        list_kdn += kg.list_kdn
-
-    # for op in fwd_op_list:
-    #     print(f'fwd_op: {op.name}, {op.main_target}')
-    #     print(f'main code: {op.main_code}')
-    #     print(f'body code: {op.body_code}')
-    #     print(f'deps_global: {op.deps_global}')
-    #     print(f'deps fake: {op.deps_fake}')
-    #     print(f'user_global: {op.users_global}')
-
-    # fwd_op_sched = OpSchedule(
-    #     fwd_op_list,
-    #     None,
-    #     kgraph_list[0].input_kdn_data,
-    #     kgraph_list[0].input_kdn_grad,
-    #     kgraph_list[-1].output_kdn_data,
-    #     list_kdn,
-    # )
-
-    # op_sched = OpSchedule(
-    #     fwd_op_list,
-    #     None,
-    #     kgraph_list[0].input_kdn_data,
-    #     kgraph_list[0].input_kdn_grad,
-    #     kgraph_list[-1].output_kdn_data,
-    #     list_kdn,
-    # )
-
-    # device = get_device()
-    # original_mod = model
-    # storage = RK_Storage(device, original_mod, dict_constants)
-
-    # dict_constants = for_rkgb.K_graph_list[0].dict_constants
-    # # for k, v in model_inputs.items():
-    # #     storage.add_val(k, v)
-
-    # compiler = Compiler(op_sched)
-
 model = SimpleCNN()
 sample = [torch.rand(1, 3, 32, 32)]
 
-model = models.vgg16()
+model = models.resnet101()
 sample = torch.rand(5, 3, 244, 244)
 # y = model(sample)
 
