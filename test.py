bd877a25e85e1e458cd1cde573cc9260292e13ff
zero1777 <tumi9325@gmail.com>
2023-10-29 00:48:14 +0800

    v2: delete unused file & combine example to test

--- test.py
+++ test.py
@@ -7,17 +7,15 @@ def pack(x):
 def unpack(x):
     return x
 
-x = torch.randn(5, requires_grad=True)
-with torch.autograd.graph.saved_tensors_hooks(pack, unpack):
-    y = torch.relu(x)
-
+# x = torch.randn(5, requires_grad=True)
+# with torch.autograd.graph.saved_tensors_hooks(pack, unpack):
+#     y = torch.relu(x)
 # print(y.equal(y.grad_fn._saved_result))  # True
 # print(y is y.grad_fn._saved_result)  # False
 
 a = torch.randn(5, requires_grad=True).to('cuda')
-# b = a.exp()
 with torch.autograd.graph.saved_tensors_hooks(pack, unpack):
-    b = torch.nn.functional.relu(a)
+    b = torch.relu(a)
 c = torch.empty(b.size(), device='cpu')
 c.copy_(b, non_blocking=True)
 c = c.detach()
@@ -31,4 +29,4 @@ b.data = torch.empty(0, device='cuda')
 
 print(torch.cuda.memory_allocated())
 
-# b.backward(torch.ones_like(a))
+b.data = c.data.cuda(non_blocking=True)
